773d1c26357228a7b74d122162bb1c2b0a8f0b55
Marcos Sanches <58822240+markitosanches@users.noreply.github.com>
2022-10-26 10:44:01 -0400

    Seance 11

---
+++ my-blog-seance-11/resources/views/auth/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-4 pt-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">
+                            Login
+                        </h3>
+                        <div class="card-body">
+                        @if(session('success'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    {{session('success')}}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                        @endif
+                            <form action="{{route('login.authentication')}}" method="post">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <input type="email" placeholder="email" class="form-control" name="email">
+                                    @if ($errors->has('email'))
+                                        <div class="text-danger mt-2">
+                                            {{$errors->first('email')}}
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="password" class="form-control" name="password">
+                                    @if ($errors->has('password'))
+                                        <div class="text-danger mt-2">
+                                            {{$errors->first('password')}}
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="d-grid mx-auto">
+                                    <input type="submit" value="Connecter" class="btn btn-dark btn-block">
+                                </div>
+                            </form>       
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
+
---
+++ my-blog-seance-11/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogPostController ;
+use App\Http\Controllers\CustomAuthController ;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/blog', [BlogPostController::class, 'index'])->name('blog.index');
+Route::get('/blog/{blogPost}', [BlogPostController::class, 'show'])->name('blog.show');
+Route::get('/blog-create', [BlogPostController::class, 'create'])->name('blog.create');
+Route::post('/blog-create', [BlogPostController::class, 'store'])->name('blog.store');
+Route::get('/blog-edit/{blogPost}', [BlogPostController::class, 'edit'])->name('blog.edit');
+Route::put('/blog-edit/{blogPost}', [BlogPostController::class, 'update'])->name('blog.update');
+Route::delete('blog-edit/{blogPost}', [BlogPostController::class, 'destroy'])->name('blog.destroy');
+
+Route::get('/query', [BlogPostController::class, 'query'])->name('blog.query');
+
+Route::get('/login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('/login', [CustomAuthController::class, 'authentication'])->name('login.authentication');
+Route::get('/registration', [CustomAuthController::class, 'create'])->name('user.registration');
+Route::post('/registration-store', [CustomAuthController::class, 'store'])->name('user.store');
